70552685c9624b8117295b8d752e0b55ec5e6853
alexandraalday <alexandra.alday@gmail.com>
2017-07-18 16:07:05 -0700

    instructions modal functional

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ console.log('loaded bro')
 
 
 
+// INSTRUCTIONS BUTTON AND MODAL
+
+const $modal = $('.modal')
+const $button = $('#instructions')
+const $close = $('.close')
 
 
+// When the user clicks on the button, open the modal
+$button.on('click', () => {
+	$modal.css('display', "block")
+});
+
+// When the user clicks on <span> (x), close the modal
+$close.on('click', () => {
+    $modal.css('display', "none")
+});
 
 
 
