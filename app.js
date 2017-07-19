b8d0b253f0dd8f29beee421cac1d61eaea0cb038
alexandraalday <alexandra.alday@gmail.com>
2017-07-19 08:29:36 -0700

    crystal gem and start button event listeners

--- app.js
+++ app.js
@@ -1,27 +1,62 @@
 $(() => {
 
 
-console.log('loaded bro')
-
+console.log('loaded bro');
 
 
 
 // INSTRUCTIONS BUTTON AND MODAL
 
-const $modal = $('.modal')
-const $button = $('#instructions')
-const $close = $('.close')
+const $modal = $('.modal');
+const $button = $('#instructions');
+const $close = $('.close');
 
 
 // When the user clicks on the button, open the modal
 $button.on('click', () => {
-	$modal.css('display', "block")
+	$modal.css('display', "block");
 });
 
 // When the user clicks on <span> (x), close the modal
 $close.on('click', () => {
-    $modal.css('display', "none")
+    $modal.css('display', "none");
 });
+
+
+
+
+// CRYSTAL GEM EVENT LISTENERS
+
+$('#top-diamond').on('click', () =>{
+	console.log('top diamond clicked');
+})
+
+$('#right-diamond').on('click', () =>{
+	console.log('right diamond clicked');
+})
+
+$('#left-diamond').on('click', () =>{
+	console.log('left diamond clicked');
+})
+
+$('#bottom-diamond').on('click', () =>{
+	console.log('bottom diamond clicked');
+})
+
+
+// START BUTTON EVENT LISTENER
+
+$('#start').on('click', () => {
+	console.log('start button clicked');
+})
+
+
+
+
+
+
+
+
 
 
 
