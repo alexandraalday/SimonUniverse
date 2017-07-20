9956b73eaca4848a168485fae534c21edd7be342
alexandraalday <alexandra.alday@gmail.com>
2017-07-19 17:29:23 -0700

    simon pushes 20 rounds of random buttons, buttons glow and play sound each instance they are pushed

--- README.md
+++ README.md
@@ -8,10 +8,11 @@ HTML, CSS, JavaScript, jQuery, Velocity JS(?)
 
 
 #### the approach taken/ technical details
-imported a local font - Crystal Universe
-background clip to text is amazeballs!
-really wanted to manually code the gems in CSS
-amazon web services to host sound bites
+-imported a local font - Crystal Universe
+-background clip to text is amazeballs!
+-really wanted to manually code the gems in CSS
+-amazon web services to host sound bites
+-keyframe animations for glow effect
 
 
 #### installation instructions
@@ -19,7 +20,7 @@ amazon web services to host sound bites
 
 
 #### unsolved problems/ technical challenges
-
+-when doing animations for hover effects, it was only active on the initial button click. Jim to the rescue with the .hover() method and toggling it active, inactive. 
 
 
 #### opportunities for improvement
--- app.js
+++ app.js
@@ -3,6 +3,118 @@ $(() => {
 
 console.log('loaded bro');
 
+// SOME GLOBAL VARIABLES
+
+let i=0;
+let strict = false;
+let round = '';
+let simonArray = [];
+let playerArray = [];
+let setSimonPlay;
+
+
+
+
+
+// GAME LOGIC
+
+
+
+const checkWinner = (e) => {
+  playerArray.push(e.currentTarget);
+  let counter = 0;
+  if (playerArray.length < i) {
+    return;
+  }
+  else if (playerArray.length === round) {
+    for (let x = 0; x < playerArray.length; x++) {
+      console.log(playerArray[x], simonArray[x]);
+      if (playerArray[x] !== simonArray[x]) {
+        if (!strict) { //player can try again
+          i = 0;
+          //display a wrong error message on the scoreboard and play a "razz" buzzer sound
+          playerArray = []; //reset player array so they can try again
+          i = 0;
+          setSimonPlay = undefined;
+          setSimonPlay = setInterval(simonPlay, 2500);
+          return;
+        } else if (strict) {
+          i = 0;
+          //display a losing message on the scoreboard and play a "razz" buzzer sound
+          playerArray = [];
+          simonArray= [];
+          round = 0;
+          return;
+        }
+      } else if (playerArray[x] === simonArray[x]) {
+          counter++
+          if (counter === 20) {
+            //display winning message to scoreboard
+            playerArray = [];
+            simonArray = [];
+            round = 0;
+            i = 0;
+            return;
+          }
+          else if (counter === round) {
+            round++;
+            //display round to scoreboard
+            i = 0;
+            setSimonPlay = undefined;
+            setSimonPlay = setInterval(simonPlay, 2500);
+            return;
+          }
+      }
+    }
+  }
+}        
+ 
+const simonPlay = () => {
+	// checkWinner();
+//display round text to screen here when create scoreboard
+  if (simonArray[i] === "top-diamond") {
+  	$('#top-sound')[0].play();
+  	$('#top-diamond').addClass("active")
+  	setInterval(function(){
+    	$('#top-diamond').removeClass("active");
+	}, 2000);
+  }
+  else if (simonArray[i] === "right-diamond") {
+  		//look into making this a function since you repeat this later in the code
+  	$('#right-sound')[0].play();
+  	$('#right-diamond').addClass("active")
+  	setInterval(function(){
+    	$('#right-diamond').removeClass("active");
+	}, 2000);
+  }
+  else if (simonArray[i] === "left-diamond") {
+  	$('#left-sound')[0].play();
+  	$('#left-diamond').addClass("active")
+  	setInterval(function(){
+    	$('#left-diamond').removeClass("active");
+	}, 2000);
+  }
+  else if (simonArray[i] === "bottom-diamond") {
+  	$('#bottom-sound')[0].play();
+  	$('#bottom-diamond').addClass("active")
+  	setInterval(function(){
+    	$('#bottom-diamond').removeClass("active");
+	}, 2000);
+  }
+  i++;
+}
+
+
+const startGame = () => {
+    //computer selects 20 random colors
+    let gemArray = ["top-diamond", "right-diamond", "left-diamond", "bottom-diamond"];
+    for (let g = 0; g < 20; g++) {
+      simonArray.push(gemArray[Math.floor(Math.random() * gemArray.length)]);
+    }
+    round = 1;
+    //display round text to screen here when create scoreboard
+    setSimonPlay = setInterval(simonPlay, 2500);
+}
 
 
 // INSTRUCTIONS BUTTON AND MODAL
@@ -29,48 +141,53 @@ $close.on('click', () => {
 });
 
 
-
-
 // CRYSTAL GEM EVENT LISTENERS
 
 $('#top-diamond').on('click', () =>{
 	console.log('top diamond clicked');
-	let $audioElement = $('<audio>');
-    $audioElement.attr('src', 'https://s3-us-west-2.amazonaws.com/simonuniversesounds/simonSound1.mp3');
-    $button.append($audioElement);
-	$audioElement[0].play();
+  	$('#top-sound')[0].play();
+    $('e.currentTarget').hover(function() {
+      $(this).toggleClass("active")
+    });
 })
 
 $('#right-diamond').on('click', () =>{
 	console.log('right diamond clicked');
-	let $audioElement = $('<audio>');
-    $audioElement.attr('src', 'https://s3-us-west-2.amazonaws.com/simonuniversesounds/simonSound2.mp3');
-    $button.append($audioElement);
-	$audioElement[0].play();
+  	$('#right-sound')[0].play();
+    $('e.currentTarget').hover(function() {
+      $(this).toggleClass("active")
+    });
 })
 
 $('#left-diamond').on('click', () =>{
 	console.log('left diamond clicked');
-    let $audioElement = $('<audio>');
-    $audioElement.attr('src', 'https://s3-us-west-2.amazonaws.com/simonuniversesounds/simonSound3.mp3');
-    $button.append($audioElement);
-	$audioElement[0].play();
+  	$('#left-sound')[0].play();
+    $('e.currentTarget').hover(function() {
+      $(this).toggleClass("active")
+    });
 })
 
 $('#bottom-diamond').on('click', () =>{
 	console.log('bottom diamond clicked');
-    let $audioElement = $('<audio>');
-    $audioElement.attr('src', 'https://s3-us-west-2.amazonaws.com/simonuniversesounds/simonSound4.mp3');
-    $button.append($audioElement);
-	$audioElement[0].play();
+  	$('#bottom-sound')[0].play();
+    $('e.currentTarget').hover(function() {
+      $(this).toggleClass("active")
+    });
 })
+
 
 
 // START BUTTON EVENT LISTENER
 
 $('#start').on('click', () => {
 	console.log('start button clicked');
+	startGame();
 })
+
+
+
+
+
 
 
 
